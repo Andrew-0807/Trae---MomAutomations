78d82a3cca8f742356ab6bd9884c5180f57e3e97
Andrew-0807 <andreeeivlad0807@gmail.com>
2025-09-16 21:49:27 +0300

    Bump version to 1.0.2

---
+++ app_info.py
@@ -0,0 +1,8 @@
+APP_NAME = "Excel Processor"
+REPO_OWNER = "Andrew-0807"
+REPO_NAME = "ExcelProcessor"
+RELEASE_ASSET_NAME = "ExcelProcessor.exe"
+DEFAULT_HOST = "127.0.0.1"
+DEFAULT_PORT = 5000
+UPDATE_CHECK_INTERVAL = 60 * 60  # seconds
+__version__ = "1.0.2"
